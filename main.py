eb87a799ea1c1a978ff919353c39b79e4fc117ea
ngandng <ngan.uet@gmail.com>
2024-07-02 20:35:31 +0900

    update code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
-import matplotlib.patches as patches
+from mpl_toolkits.mplot3d import Axes3D
 
 from robot import *
 
@@ -10,47 +10,73 @@ X_MIN = 0; X_MAX = 100
 Y_MIN = 0; Y_MAX = 100
 Z_MIN = 0; Z_MAX = 30
 
-
 # Parameters
-start = (0, 0, np.pi/4)  # Starting position (x, y, theta)
-goal = (95, 95)       # Goal position (x, y)
-Kp_linear = 1.0     # Proportional gain for linear velocity
-Kp_angular = 2.0    # Proportional gain for angular velocity
-dt = 1            # Time step
-max_steps = 50      # Maximum number of simulation steps
-robot_radius = 0.2  # Radius of the robot circle
+start = (0, 0, np.pi/4)     # Starting position (x, y, theta)
+goal = (200, 200)             # Goal position (x, y)
+Kp_linear = 1.0             # Proportional gain for linear velocity
+Kp_angular = 2.0            # Proportional gain for angular velocity
+dt = 1                      # Time step
+max_steps = 100              # Maximum number of simulation steps
+robot_radius = 0.2          # Radius of the robot circle
+robot_sensing_range = 20     # 
+
+class Environment:
+    def __init__(self):
+        self.xmax = X_MAX
+        self.xmin = X_MIN
+        self.ymax = Y_MAX
+        self.ymin = Y_MIN
+        self.zmax = Z_MAX
+        self.zmin = Z_MIN
 
 def simulate_robot(start, goal, Kp_linear, Kp_angular, dt, max_steps):
-    robot = DifferentialDriveRobot(start[0], start[1], start[2])
+    robot = DifferentialDriveRobot(start[0], start[1], start[2], vmax=5, sr=robot_sensing_range)
     trajectory = [(robot.x, robot.y)]
+    task_list = []
+
+    env = Environment()
 
     for _ in range(max_steps):
         v, omega = calculate_control(robot, goal, Kp_linear, Kp_angular)
-        robot.update_position(v, omega, dt)
+
+        # update new position and sense for new tasks
+        robot.update_position(v, omega, dt, env)
+        
         trajectory.append((robot.x, robot.y))
+
+        task_list.append(robot.task)
         
         # Stop if the robot is close enough to the goal
         if np.linalg.norm([robot.x - goal[0], robot.y - goal[1]]) < 0.01:
             break
     
-    return trajectory
+    return trajectory, task_list
 
 # Simulate the robot
-trajectory = simulate_robot(start, goal, Kp_linear, Kp_angular, dt, max_steps)
+trajectory, task_list = simulate_robot(start, goal, Kp_linear, Kp_angular, dt, max_steps)
 
+print('taks_list.len ', len(task_list))
+for i in range(len(task_list)):
+    print(task_list[i].shape)
 # Plotting the trajectory as an animation
 trajectory = np.array(trajectory)
-fig, ax = plt.subplots()
-ax.set_xlim((min(trajectory[:,0])-1, max(trajectory[:,0])+1))
-ax.set_ylim((min(trajectory[:,1])-1, max(trajectory[:,1])+1))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim((min(trajectory[:, 0]) - 1, max(trajectory[:, 0]) + 1))
+ax.set_ylim((min(trajectory[:, 1]) - 1, max(trajectory[:, 1]) + 1))
+ax.set_zlim(0, 30)
 line, = ax.plot([], [], 'b-', label='Robot Trajectory')
 start_marker, = ax.plot([], [], 'bo', label='Start')
 goal_marker, = ax.plot([], [], 'ro', label='Goal')
-robot_circle, = ax.plot([], [], 'ys', label='Robot',markersize=20)
+robot_circle, = ax.plot([], [], 'ys', label='Robot', markersize=20)
+robot_direction, = ax.plot([], [], 'r-')
+task_scatter, = ax.plot([], [], [], c='g', marker='o', label='Task', linestyle='', markersize=7)
 
 ax.set_xlabel('X Position')
 ax.set_ylabel('Y Position')
-ax.legend()
+ax.set_zlabel('Z Position')
+ax.legend(loc='upper left')
 ax.set_title('Trajectory of Differential Drive Robot')
 ax.grid(True)
 
@@ -59,12 +85,33 @@ def init():
     start_marker.set_data([start[0]], [start[1]])
     goal_marker.set_data([goal[0]], [goal[1]])
     robot_circle.set_data([start[0]], [start[1]])
-    return line, start_marker, goal_marker, robot_circle
+    robot_direction.set_data([], [])
+    task_scatter.set_data([], [], [])
+
+    return line, start_marker, goal_marker, robot_circle, robot_direction, task_scatter
 
 def update_frame(frame):
     line.set_data(trajectory[:frame, 0], trajectory[:frame, 1])
     robot_circle.set_data([trajectory[frame, 0]], [trajectory[frame, 1]])
-    return line, start_marker, goal_marker, robot_circle
+
+    # Calculate the direction line based on robot's theta
+    direction_length = 5.0
+    direction_x = trajectory[frame, 0] + direction_length * np.cos(start[2])
+    direction_y = trajectory[frame, 1] + direction_length * np.sin(start[2])
+    robot_direction.set_data([trajectory[frame, 0], direction_x],
+                             [trajectory[frame, 1], direction_y])
+
+    if frame < len(task_list):
+        tasks = task_list[frame]
+        if len(tasks) > 0:
+            tx = tasks[:, 0]
+            ty = tasks[:, 1]
+            tz = tasks[:, 2]
+            task_scatter.set_data(tx, ty, tz)
+        else:
+            task_scatter.set_data([], [], [])
+
+    return line, start_marker, goal_marker, robot_circle, robot_direction, task_scatter
 
 ani = FuncAnimation(fig, update_frame, frames=len(trajectory), init_func=init, blit=True, repeat=False)
 
--- robot.py
+++ robot.py
@@ -1,30 +1,56 @@
 import numpy as np
 
-# Define the environment
-X_MIN = 0; X_MAX = 100
-Y_MIN = 0; Y_MAX = 100
-Z_MIN = 0; Z_MAX = 30
-
-
-# Parameters
-start = (0, 0, np.pi/4)  # Starting position (x, y, theta)
-goal = (95, 95)       # Goal position (x, y)
-Kp_linear = 1.0     # Proportional gain for linear velocity
-Kp_angular = 2.0    # Proportional gain for angular velocity
-dt = 1            # Time step
-max_steps = 50      # Maximum number of simulation steps
-robot_radius = 0.2  # Radius of the robot circle
-
 class DifferentialDriveRobot:
-    def __init__(self, x, y, theta):
+    def __init__(self, x, y, theta, vmax, sr):
         self.x = x
         self.y = y
+        self.z = 0
         self.theta = theta
 
-    def update_position(self, v, omega, dt):
+        self.vmax = vmax
+        
+        self.sensing_range = sr
+        self.task = np.empty((1,3))
+
+    def update_position(self, v, omega, dt, env):
         self.x += v * np.cos(self.theta) * dt
         self.y += v * np.sin(self.theta) * dt
         self.theta += omega * dt
+
+        self.sense_new_task(env)
+        
+    def add_task(self, new_task):
+        # if(new_task):
+        self.task = np.vstack([self.task, new_task])
+        
+    def sense_new_task(self, env):
+        # based on robot.x, robot.y and robot.theta, gen a new task
+
+        """PARAMETER: modify the number bellow to decide density of the tasks"""
+        if(np.random.rand()<0.7):
+            """
+            The task position is randomly generated
+            within the robot's sensing range and within
+            a certain angle from its current heading.
+
+            Here each step just add 1 or 0 new task. We can add more
+            """
+            # task = [x, y, z]
+            z = np.random.uniform(env.zmax-20, env.zmax)
+
+            dis = np.random.rand()*self.sensing_range
+            sen_angle = np.random.uniform(-np.pi/2,np.pi/2)
+
+            print("generate a task with sensing_angle ", np.degrees(sen_angle))
+
+            x = self.x + dis * np.cos(self.theta+sen_angle)
+            y = self.y + dis * np.sin(self.theta+sen_angle)
+
+            task = np.array([x, y, z])
+
+            print('Add task at location ',x,y,z)
+            self.add_task(task)
+            # print("addded new task")
 
 def calculate_control(robot, goal, Kp_linear, Kp_angular):
     # Calculate the error in position
@@ -44,18 +70,11 @@ def calculate_control(robot, goal, Kp_linear, Kp_angular):
     # Proportional control for linear and angular velocities
     v = Kp_linear * distance_error
     omega = Kp_angular * orientation_error
+
+    if v>robot.vmax:
+        v = robot.vmax
     
     return v, omega
-
-def sense_new_task(robot):
-    # based on robot.x, robot.y and robot.theta, gen a new task
-    # task = [x, y, z]
-    z = np.random.uniform(Z_MAX-20, Z_MAX)
-    dis = np.random
-    x += dis * np.cos(robot.theta)
-    y += dis * np.sin(robot.theta)
-    task = [robot.x, robot.y, z]
-    return task
 
 def find_opposite_and_furthest_points(robot_position, velocity, points):
     p = np.array(robot_position)
